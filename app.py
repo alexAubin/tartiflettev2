8c80099bd77d11562c93e0477fc9f8cb82cba748
Alexandre Aubin <alex.aubin@mailoo.org>
2024-05-01 16:37:21 +0200

    Black

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ from flask_babel import gettext as _
 
 app = Flask(__name__, static_url_path="/assets", static_folder="assets")
 
-#try:
+# try:
 #    config = toml.loads(open("config.toml").read())
-#except Exception as e:
+# except Exception as e:
 #    print(
 #        "You should create a config.toml with the appropriate key/values, cf config.toml.example"
 #    )
@@ -30,19 +30,23 @@ if app.config.get("DEBUG"):
 
 AVAILABLE_LANGUAGES = ["en"] + os.listdir("translations")
 
+
 @app.template_filter("locale")
 def get_locale(*args, **kwargs):
     # try to guess the language from the user accept
     # The best match wins.
     return request.accept_languages.best_match(AVAILABLE_LANGUAGES) or "en"
 
+
 babel = Babel(app, locale_selector=get_locale)
 
 ###############################################################################
 
+
 @app.template_filter("days_ago")
 def days_ago(timestamp):
-    return int((time.time() - timestamp) / (60*60*24))
+    return int((time.time() - timestamp) / (60 * 60 * 24))
+
 
 ###############################################################################
 
@@ -55,6 +59,5 @@ def favicon():
 @app.route("/")
 def index():
     return render_template(
-        "index.html",
-        data=json.loads(open(".cache/dashboard.json").read())
+        "index.html", data=json.loads(open(".cache/dashboard.json").read())
     )
--- fetch.py
+++ fetch.py
@@ -21,8 +21,12 @@ g = Github(github_token)
 
 popularity_stars = requests.get("https://apps.yunohost.org/popularity.json").json()
 catalog = requests.get("https://app.yunohost.org/default/v3/apps.json").json()
-main_ci_apps_results = requests.get("https://ci-apps.yunohost.org/ci/api/results").json()
-nextdebian_ci_apps_results = requests.get("https://ci-apps-bookworm.yunohost.org/ci/api/results").json()
+main_ci_apps_results = requests.get(
+    "https://ci-apps.yunohost.org/ci/api/results"
+).json()
+nextdebian_ci_apps_results = requests.get(
+    "https://ci-apps-bookworm.yunohost.org/ci/api/results"
+).json()
 
 
 def get_github_infos(github_orga_and_repo):
@@ -38,11 +42,19 @@ def get_github_infos(github_orga_and_repo):
     testings = [p for p in pulls if p.head.ref == "testing"]
     testing = testings[0] if testings else None
     ci_auto_updates = [p for p in pulls if p.head.ref.startswith("ci-auto-update")]
-    ci_auto_update = sorted(ci_auto_updates, key=lambda p: p.created_at, reverse=True)[0] if ci_auto_updates else None
+    ci_auto_update = (
+        sorted(ci_auto_updates, key=lambda p: p.created_at, reverse=True)[0]
+        if ci_auto_updates
+        else None
+    )
 
-    for p in ([testing] if testing else []) + ([ci_auto_update] if ci_auto_update else []):
+    for p in ([testing] if testing else []) + (
+        [ci_auto_update] if ci_auto_update else []
+    ):
 
-        if p.head.label != "YunoHost-Apps:testing" and not (p.user.login == "yunohost-bot" and p.head.ref.startswith("ci-auto-update-")):
+        if p.head.label != "YunoHost-Apps:testing" and not (
+            p.user.login == "yunohost-bot" and p.head.ref.startswith("ci-auto-update-")
+        ):
             continue
 
         infos["testing" if p.head.ref == "testing" else "ci-auto-update"] = {
@@ -51,12 +63,18 @@ def get_github_infos(github_orga_and_repo):
             "timestamp_created": int(p.created_at.timestamp()),
             "timestamp_updated": int(p.updated_at.timestamp()),
             "statuses": [
-                {"state": s.state, "context": s.context, "url": s.target_url, "timestamp": int(s.updated_at.timestamp())}
+                {
+                    "state": s.state,
+                    "context": s.context,
+                    "url": s.target_url,
+                    "timestamp": int(s.updated_at.timestamp()),
+                }
                 for s in repo.get_commit(p.head.sha).get_combined_status().statuses
-            ]
+            ],
         }
 
     return infos
+
 
 consolidated_infos = {}
 for app, infos in catalog["apps"].items():
@@ -75,18 +93,30 @@ for app, infos in catalog["apps"].items():
         "packaging_format": infos["manifest"]["packaging_format"],
         "popularity_stars": popularity_stars.get(app, 0),
         "ci_results": {
-            "main": {
-                "level": main_ci_apps_results[app]["level"],
-                "timestamp": main_ci_apps_results[app]["timestamp"]
-            } if app in main_ci_apps_results else None,
-            "nextdebian": {
-                "level": nextdebian_ci_apps_results[app]["level"],
-                "timestamp": nextdebian_ci_apps_results[app]["timestamp"]
-            } if app in nextdebian_ci_apps_results else None
-        }
+            "main": (
+                {
+                    "level": main_ci_apps_results[app]["level"],
+                    "timestamp": main_ci_apps_results[app]["timestamp"],
+                }
+                if app in main_ci_apps_results
+                else None
+            ),
+            "nextdebian": (
+                {
+                    "level": nextdebian_ci_apps_results[app]["level"],
+                    "timestamp": nextdebian_ci_apps_results[app]["timestamp"],
+                }
+                if app in nextdebian_ci_apps_results
+                else None
+            ),
+        },
     }
 
     if infos["git"]["url"].lower().startswith("https://github.com/"):
-        consolidated_infos[app].update(get_github_infos(infos["git"]["url"].lower().replace("https://github.com/", "")))
+        consolidated_infos[app].update(
+            get_github_infos(
+                infos["git"]["url"].lower().replace("https://github.com/", "")
+            )
+        )
 
 open(".cache/dashboard.json", "w").write(json.dumps(consolidated_infos))
