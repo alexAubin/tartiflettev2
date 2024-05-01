716753ff676ba36019ea4b586d8775a6f882891f
Alexandre Aubin <alex.aubin@mailoo.org>
2024-05-01 18:48:32 +0200

    Cache dashboard data instead of re-reading the json at every request

--- app.py
+++ app.py
@@ -54,21 +54,34 @@ def favicon():
     return send_from_directory("assets", "favicon.png")
 
 
+def get_dashboard_data():
+    path = ".cache/dashboard.json"
+    mtime = os.path.getmtime(path)
+    if get_dashboard_data.mtime != mtime:
+        get_dashboard_data.mtime = mtime
+        dashboard_data = json.load(open(path))
+        get_dashboard_data.cache = dashboard_data
+
+    return get_dashboard_data.cache
+
+get_dashboard_data.mtime = None
+get_dashboard_data()
+
 @app.route("/")
 def index():
     return render_template(
-        "index.html", data=json.loads(open(".cache/dashboard.json").read())
+        "index.html", data=get_dashboard_data()
     )
 
 
 @app.route("/charts")
 def charts():
 
-    current = json.loads(open(".cache/dashboard.json").read())
+    dashboard_data = get_dashboard_data()
     level_summary = {}
     for i in range(0,9):
-        level_summary[i] = len([infos for infos in current.values() if infos.get("ci_results", {}).get("main").get("level") == i])
-    level_summary["unknown"] = len([infos for infos in current.values() if infos.get("ci_results", {}).get("main").get("level") in [None, "?"]])
+        level_summary[i] = len([infos for infos in dashboard_data.values() if infos.get("ci_results", {}).get("main").get("level") == i])
+    level_summary["unknown"] = len([infos for infos in dashboard_data.values() if infos.get("ci_results", {}).get("main").get("level") in [None, "?"]])
 
     return render_template(
         "charts.html",
